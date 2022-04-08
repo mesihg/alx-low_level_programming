65004ca7358b81f2090a5a331df29f69073482a1
Mesih Grmay <mesihgrmay@yahoo.com>
2022-04-08 13:44:00 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+for(int i = 0; i < 10; i++)
+{
+putchar(i);
+}
+putchar('\n');
+
+return (0);
+}
