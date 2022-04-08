56811dd0cd3c15b4b613fa2afb67480148b3fe08
Mesih Grmay <mesihgrmay@yahoo.com>
2022-04-08 13:40:15 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,15 +10,15 @@ int main(void)
 char alphabet;
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
- if (alphabet == 'q')
- {
- continue;
- }
- else if (alphabet == 'e')
- {
- continue;
- }
- putchar(alphabet);
+if (alphabet == 'q')
+{
+continue;
+}
+else if (alphabet == 'e')
+{
+continue;
+}
+putchar(alphabet);
 }
 putchar('\n');
 
