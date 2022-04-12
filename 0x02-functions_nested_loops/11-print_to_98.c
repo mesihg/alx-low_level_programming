ab34601571341901d419cf36d25811f5cd6aa7e5
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-04-13 01:20:57 +0300

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,29 +8,16 @@
  */
 void print_to_98(int n)
 {
-int i;
-if (n > 98)
+if (n < 98)
 {
-for (i = n; i > 97; i--)
-{
-printf("%d",i);
-if (i != 98)
-{
-printf(", ");
-}
-}
+for (n = n; n < 98; n++)
+printf("%d, ", n);
+printf("%d\n", 98);
 }
 else
 {
-for (i = n; i < 99; i++)
-{
-printf("%d",i);
-if (i != 98)
-{
-printf(", ");
+for (n = n; n > 98; n--)
+printf("%d, ", n);
+printf("%d\n", 98);
 }
-}
-}
-printf('\n');
-return (0);
 }
