f7cfa107df3fa323da41c14a88681352d4d7d05a
Mesih Grmay <mesihgrmay@yahoo.com>
2022-04-12 13:45:34 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet)x10 - prints alphabets in lowercase 10 times
+ */
+void print_alphabet_x10(void)
+{
+    int letter;
+    int count;
+
+    for (count = 0; count < 10; count++)
+    {
+            for (letter = 'a'; letter <= 'z'; letter++)
+		    _putchar(letter);
+	    _putchar('\n');
+    }
+
+}
