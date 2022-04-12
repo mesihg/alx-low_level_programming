77b71128b1354eb4ec4ae331b6b9f212e6f00384
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-04-12 23:33:46 +0300

    Task 0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,16 +5,12 @@
  *
  * Return: Always 0 (success)
  */
-
 void print_alphabet(void)
 {
 char letter;
-
 for (letter = 'a'; letter <= 'z'; letter++)
 {
 _putchar(letter);
 }
-
 _putchar('\n');
-
 }
