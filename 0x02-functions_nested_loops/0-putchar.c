4f20b4d98648b83c4125b1da2650f26c716aef3d
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-04-12 23:16:20 +0300

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,15 @@
  */
 int main(void)
 {
-char string[] = "_putchar";
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-for(int i = 0; i < 8; i++){
-_putchar(string[i]);
-}
-_putchar("\n");
 return (0);
-
 }
