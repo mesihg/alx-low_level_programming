502008ee2c295a7e958c412ced7c4bd0eab15fd0
Mesih Grmay <mesihgrmay@yahoo.com>
2022-04-12 13:10:55 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,11 @@
  */
 int main(void)
 {
-_putchar("_");
-_putcahr("p");
-_putchar("u");
-_putchar("t");
-_putchar("c");
-_putchar("h");
-_putchar("a");
-_putchar("r");
+char string[] = "_putchar";
+
+for(int i = 0; i < 8; i++){
+_putchar(string[i]);
+}
 _putchar("\n");
 return (0);
 
