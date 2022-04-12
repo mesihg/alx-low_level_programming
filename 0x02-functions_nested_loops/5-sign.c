ace872d378847ede42f3f966abd98a251cfa5aef
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-04-13 00:17:58 +0300

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,9 +2,10 @@
 
 /**
  * print_sign - prints sign of a number
- * @c: int input to be compared
+ * @c: int n input to be compared
  * Return: always 0 (Success)
  */
+
 int print_sign(int n)
 {
 
@@ -13,11 +14,13 @@ if (n > 0)
 _putchar('+');
 return (1);
 }
+
 else if (n == 0)
 {
 _putchar('0');
 return (0);
 }
+  
 else 
 {
 _putchar('-');
