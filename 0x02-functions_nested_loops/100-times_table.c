bc015a60029d14ad7b92aad1c10a2d88c258e318
Mesih Grmay <mesihgrmay@yahoo.com>
2022-04-13 12:28:48 -0700

    Task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,25 @@
+#include <main.h>
+
+/**
+ * print_times_table - prints n times table
+ *
+ * @n: integer input n
+ *
+ * Return: Always 0
+ */
+void print_times_table(int n)
+{
+int i;
+
+if (n >= 0 && n <= 15)
+{
+
+for (i = 0; i <= n; i++)
+{
+
+
+}
+
+}
+
+}
