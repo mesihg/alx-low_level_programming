32fab6feef16036d4a3b7b86b06869976a558e91
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-04-14 01:14:52 +0300

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,18 +6,36 @@
  */
 int main(void)
 {
-long int i, n1 = 1, n2 = 2, fib = 0;
+unsigned long int i, j, k, j1, j2, k1, k2;
 
-for (i = 0; i < 98; i++)
+j = 1;
+k = 2;
+
+printf("%lu", j);
+
+for (i = 1; i < 91; i++)
 {
-printf("%ld, ", n1);
-fib = n1 + n2;
-n1 = n2;
-n2 = fib;
-if (i == 97)
+printf(", %lu", k);
+k = k + j;
+j = k - j;
+}
+
+j1 = j / 1000000000;
+j2 = j % 1000000000;
+k1 = k / 1000000000;
+k2 = k % 1000000000;
+
+for (i = 92; i < 99; ++i)
 {
-printf("%ld\n", n1);
+printf(", %lu", k1 + (k2 / 1000000000));
+printf("%lu", k2 % 1000000000);
+k1 = k1 + j1;
+j1 = k1 - j1;
+k2 = k2 + j2;
+j2 = k2 - j2;
 }
-}
+
+printf("\n");
+
 return (0);
 }
