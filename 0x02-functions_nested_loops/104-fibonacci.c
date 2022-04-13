540a4e16601aa0068d4328fe2f0cd86f6208dfb2
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-04-14 00:55:28 +0300

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-int i, n1 = 1, n2 = 2, fib;
+int i, n1 = 1, n2 = 2, fib = 0;
 
-for (i = 0; i < 98; i++)
+for (i = 1; i < 98; i++)
 {
 if (i == 97)
 {
-printf("%d", n1);
+printf("%d\n", n1);
 }
 else
 {
