738e94c37073a184d9ee8ac01db5e2d3a6da2d63
Mesih Grmay <mesihgrmay@yahoo.com>
2022-04-13 12:43:34 -0700

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n1 = 0, n2 = 1, fib = 0, sum = 0;
+
+while (fib <= 4000000)
+{
+fib = n1 + n2;
+n1 = n2;
+n2 = fib;
+
+if ((n % 2) == 0)
+	sum += n1;
+}
+printf("%d\n", sum);
+return (0);
+}
