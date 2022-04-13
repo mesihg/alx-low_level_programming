a4eb76ff6d7c859c69660909b761343d27a4c5ad
Mesih Grmay <mesihgrmay@yahoo.com>
2022-04-13 14:44:30 -0700

    Task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i, n1 = 0, n2 = 1, fib, sum = 0;
+
+for (i = 0; i < 98; i++)
+{
+if (i == 97)
+{
+printf("%d",n1); 
+}
+else
+{
+printf("%d, ",n1);
+fib = n1 + n2;
+n1 = n2;
+n2 = fib;
+}
+
+
+}
+printf("\n");
+return (0);
+}
