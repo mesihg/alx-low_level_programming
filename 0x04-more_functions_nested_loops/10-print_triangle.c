20c3dec9af285d2c2f3db48d32cad1fd351b7ed5
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-04-15 01:08:22 +0300

    Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+void print_triangle(int size)
+{
+	int i, j, gap;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (gap = 1; gap <= (size - i); gap++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
