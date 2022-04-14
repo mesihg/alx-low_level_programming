9b361916291300cd5074f26c1624f3bc0bb1eaef
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-04-15 02:47:51 +0300

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
-* main - Entry point
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 
 int main(void)
 {
@@ -11,6 +12,5 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
