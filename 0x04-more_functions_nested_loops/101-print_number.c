7e2cc89563dd832f0f99bafa90b4ae454ea02b38
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-04-15 01:45:54 +0300

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,20 +7,27 @@
 
 void print_number(int n)
 {
-unsigned int n1 = 0;
+	unsigned int x, y, count;
 
-if  (n < 0)
-{
-n1 = -n;
-_putchar('-');
-}
-else
-{
-n1 = n;
-}
-if (n1 / 10)
-{
-print_number(n1 / 10);
-}
-_putchar((n1 % 10) + '0');
+	if (n < 0)
+	{
+		_putchar(45);
+		x = -n;
+	}
+	else
+	{
+		x = n;
+	}
+	y = x;
+	count = 1;
+
+	while (y > 9)
+	{
+		y /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((x / count) % 10) + 48);
+	}
 }
