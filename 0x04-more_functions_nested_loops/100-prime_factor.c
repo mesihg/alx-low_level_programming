f4b29be94bdb9f4ed963f5821694fad1ee479630
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-04-15 01:32:41 +0300

    Task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-
-
+long int a = 612852475143; 
+long int x;
+for (x = 2; x < a; x++)
+{
+if (a % x == 0)
+{
+a = a/x;
+}
+}
+printf("%ld\n", x);
+return (0);
 }
