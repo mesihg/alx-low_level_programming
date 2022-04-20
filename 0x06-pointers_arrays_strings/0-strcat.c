f50a8194e8e8fdf2f6ff911f992c25258d88756c
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-04-20 22:00:08 +0300

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,22 +10,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-int i, j;
+	int i, c;
 
-i = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
+	for (i = 0; dest[i] != '\0'; i++)
+		;
 
-j = 0;
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-j++;
-i++;
-}
-dest[i] = '\0';
-
-return (dest);
+	for (c = 0; src[c] != '\0'; c++)
+	{
+		dest[i] = src[c];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
