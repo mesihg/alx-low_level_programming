6f08de7763b5d7d542691c6343bfa97a8e54fc63
Mesih Grmay <mesihgrmay@yahoo.com>
2022-04-21 15:05:22 -0700

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @str: a pointer for input type char str
+ *
+ * Return: encodede string
+ */
+
+char *rot13(char *str)
+{
+
+
+}
