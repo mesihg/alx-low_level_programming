14f11e06fee9288133870ad74fb9da8cee2c7fb1
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-04-21 21:44:10 +0300

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,26 +2,25 @@
 
 /**
  * leet - encodes a string
- * @str: a pointer for input type char str
+ * @s: a pointer for input type char str
  *
  * Return: returns encoded string
  */
 
-char *leet(char *str)
+char *leet(char *s)
 {
+	int i, j;
+	char c[] = "AEOTL";
+	char sm[] = "aeotl";
+	char num[] = "43071";
 
-int i, j;
-char st[] = "aAeEoOtTlL";
-char num[] = "43071";
-
-for (i = 0; str[i] != '\0'; i++)
-{
-for (j = 0; j < 5; j++)
-{
-if (str[i] == st[j])
-str[i] = num[j];
-}
-}
-	
-return (str);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == c[j] || s[i] == sm[j])
+				s[i] = num[j];
+		}
+	}
+return (s);
 }
