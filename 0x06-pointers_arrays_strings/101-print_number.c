5756432680a22e8750065ee9dc66ef3769b4f541
Mesih Grmay <mesihgrmay@yahoo.com>
2022-04-21 14:46:54 -0700

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,5 +9,18 @@
 
 void print_number(int n)
 {
-_putchar(n);
+unsigned int n1;
+n1 = n;
+
+if (n < 0)
+{
+_putchar('-');
+n1 = -n;
+}
+if (n1 / 10 != 0)
+{
+print_number(n1/10);
+}
+
+_putchar((n1 % 10) + '0');
 }
