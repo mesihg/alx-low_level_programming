36a7f4111e2cd4ef4d477a957ba17d80c79149ca
Mesih Grmay <mesihgrmay@yahoo.com>
2022-04-25 13:46:47 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strpbrk - search a string for the given string
+ * @s: a pointer for a string to be scanned
+ * @accept: a pointer for a string to be searched
+ *
+ * Return: returns the matched string
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+
+
+}
