71c5dcb7a4a23945be7ed77ee46d0995feada763
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-04-25 23:31:42 +0300

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,26 +7,18 @@
  *
  * Return: returns located substring
  */
-
 char *_strstr(char *haystack, char *needle)
 {
-int k;
+	int k;
 
-while (*haystack != '\0')
-{
-k = 0;
-while (*haystack == *needle && *haystack != '\0' && *needle != '\0')
-{
-haystack++;
-needle++;
-k++;
-}
-if (*needle == '\0')
-{
-return (haystack - k);
-}
-haystack = haytack - (k - 1);
-needle = needle - k;
-}
-return ('\0');
+	while (*haystack != '\0')
+	{
+		k = 0;
+		while (*haystack == *needle && *haystack != '\0' && *needle != '\0')
+			haystack++, needle++, k++;
+		if (*needle == '\0')
+			return (haystack - k);
+		haystack -= (k - 1), needle -= k;
+	}
+	return ('\0');
 }
