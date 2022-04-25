882438fa5f8da651e674253d00daedf438141c69
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-04-25 23:47:50 +0300

    Task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,6 +10,23 @@
 
 char *_strpbrk(char *s, char *accept)
 {
+	int j;
 
+	while (*s != '\0') /*Declaring WHILE*/
+	{
+		j = 0;
+		while (accept[j] != '\0')  /*Evaluating *accept*/
+		{
+			if (*s == accept[j])
+			{
+				return (s);
+			}
+
+			j++; /*add j+1*/
+		}
+
+		s++; /*add s+1*/
+	}
+	return (0);
 
 }
