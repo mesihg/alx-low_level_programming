f0cd0eab19c34909f4ea4410a11e64d86de244b4
Mesih Grmay <mesihgrmay@yahoo.com>
2022-04-27 05:42:22 -0700

    Task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - square root of a natural number
+ * @n: input number
+ *
+ * Return: return square root of a natural number
+ */
+
+int _sqrt_recursion_helper(int n, int i)
+{
+if (n == 0 || n == 1)
+	return (n);
+else if (i * i < n)
+	return (_sqrt_recursion_helper(i + 1, n));
+else if (i * i == n)
+	return (i);
+return (-1);
+}
+
+int _sqrt_recursion(int n)
+{
+return _sqrt_recursion_helper(n,1);
+}
