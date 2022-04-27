206809e42ee307ce5be3522185bbbb512b3ba6e8
Mesih Grmay <mesihgrmay@yahoo.com>
2022-04-27 03:10:31 -0700

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen_recursion - length of the given string
- * @s: a pointer for input string 
+ * @s: a pointer for input string
  *
  * Return: return length of the string
  */
@@ -14,5 +14,5 @@ if (*s != '\0')
 {
 len = 1 + _strlen_recursion(s + 1);
 }
-return len;
+return (len);
 }
