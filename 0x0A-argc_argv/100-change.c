a7d5e4ca7b878622dfb8ab374c7b3ee849b9e6e8
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-04-29 21:51:32 +0300

    Task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,26 +3,6 @@
 #include <stdlib.h>
 
 /**
- * isNumber - checks if @s is a number
- * @s: a pointer for char s
- *
- * Return: return 1 is its a number 0 otherwise
- */
-
-int isNumber(char *s)
-{
-int i;
-i = strlen(s);
-while (i--)
-{
-if (s[i] >= '0' && s[i] <= '9')
-	continue;
-return (0);
-}
-return (1);
-}
-
-/**
  * main - entry point
  * @argc: number of arguments
  * @argv: array of pointers
@@ -32,36 +12,41 @@ return (1);
 
 int main(int argc, char *argv[])
 {
-int n, i, cns, d, cents;
-n = 0, i = 0, cns = 0;
-int coins[5] = {25, 10, 5, 2, 1};
-if (argc > 1)
-{
-if (isNumber(argv[1]))
-{
-n = 1;
-cents = atoi(argv[1]);
-if (cents >= 0)
-{
-while (cents != 0)
-{
-d = cents / coins[i];
-if (d == 0)
-{
-i++;
-}
-else
-{
-cns += d;
-cents -= (d *coins[i]);
-}
-}
-}
-}
-}
-if (n == 0)
-	printf("%i\n", cns);
-else
-	printf("%s\n", "Error");
-return (n);
+	int change, num;
+
+	change = 0;
+	if (argc == 2)
+	{
+		num = atoi(argv[1]);
+		if (num > 0)
+		{
+			for (; num >= 25; change++)
+			{
+				num = num - 25;
+			}
+			for (; num >= 10; change++)
+			{
+				num = num - 10;
+			}
+			for (; num >= 5; change++)
+			{
+				num = num - 5;
+			}
+			for (; num >= 2; change++)
+			{
+				num = num - 2;
+			}
+			for (; num >= 1; change++)
+			{
+				num = num - 1;
+			}
+		}
+		printf("%d\n", change);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 }
