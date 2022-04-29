41210f8695024749b6a63557f9240dba163ed664
Mesih Grmay <mesihgrmay@yahoo.com>
2022-04-29 02:18:52 -0700

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
+
+/**
+ * isNumber - checks if @s is a number
+ * @s: a pointer for char s
+ *
+ * Return: return 1 is its a number 0 otherwise
+ */
+
+int isNumber(char *s)
+{
+int i;
+i = strlen(s);
+while (i--)
+{
+if(s[i] > 47 && s[i] < 58)
+	continue;
+return (0);
+}
+return (1);
+}
 
 /**
  * main - entry point
@@ -20,27 +41,16 @@ if (argc > 1)
 {
 for (i = 1; i < argc; i++)
 {
-if (isdigit(argv[i]))
-{
-result += atoi(argv[i]);
-}
+if (isNumber(argv[i]))
+	result += atoi(argv[i]);
 else
-{
-n = 1;
-break;
+	n = 1;
 }
-
 }
-
-}
-
-else if (n == 0)
-{
-printf("%i\n", result);
-}
+if (n == 0)
+	printf("%i\n", result);
 else
-{
-printf("%s\n", "Error");
-}
+        printf("%s\n", "Error");
+
 return (n);
 }
