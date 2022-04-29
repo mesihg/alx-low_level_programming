39502638c0e62d8d1df354c149dc40a3c36d2823
Mesih Grmay <mesihgrmay@yahoo.com>
2022-04-29 03:22:44 -0700

    Task 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of pointers
+ *
+ * Return: return 1 (error) or 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+
+}
