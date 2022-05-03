5b1546d9f91518fa648511d9180c96867d14d590
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-03 13:25:11 -0700

    Task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,75 +2,81 @@
 #include <stdlib.h>
 
 /**
- * countWordNum - count number of words in a given string
- * @s: char pointer input
+ * countWordNum - count number of words in a string
+ * @s: char pointer for string input
  *
- * Return: returns number of words
+ * Return: returns number of words 
  */
+
 int countWordNum(char *s)
 {
-    int i, n = 0;
+int i, n;
 
-    for (i = 0; s[i]; i++)
-    {
-        if (s[i] == ' ')
-        {
-            if (s[i + 1] != ' ' && s[i + 1] != '\0')
-                n++;
-        }
-        else if (i == 0)
-            n++;
-    }
-    n++;
-    return (n);
+n = 0;
+for (i = 0; s[i]; i++)
+{
+if (s[i] == ' ')
+	if (s[i + 1] != ' ' && s[i + 1] != '\0')
+		n++;
+else if (i == 0)
+	n++;
+}
+n++;
+return (n);
 }
 
 /**
- * strtow - splits string into words
- * @str: char pointer
+ * strtow - split string into words
+ * @str: char pointer for string input
  *
- * Return: return split words
+ * Rturn: return split words
  */
-char **strtow(char *str)
-{
-    int i, j, k, l, n = 0, wc = 0;
-    char **w;
 
-    if (str == NULL || *str == '\0')
-        return (NULL);
-    n = countWordNum(str);
-    if (n == 1)
-        return (NULL);
-    w = (char **)malloc(n * sizeof(char *));
-    if (w == NULL)
-        return (NULL);
-    w[n - 1] = NULL;
-    i = 0;
-    while (str[i])
-    {
-        if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-        {
-            for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
-                ;
-            j++;
-            w[wc] = (char *)malloc(j * sizeof(char));
-            j--;
-            if (w[wc] == NULL)
-            {
-                for (k = 0; k < wc; k++)
-                    free(w[k]);
-                free(w[n - 1]);
-                free(w);
-                return (NULL);
-            }
-            for (l = 0; l < j; l++)
-                w[wc][l] = str[i + l];
-            w[wc][l] = '\0';
-            wc++;
-            i += j;
-        }
-        else
-            i++;
-    }
-    return (w);
+char *strtow(char *str)
+{
+int i, j, k ,l, n, wc;
+char **ptr;
+
+if (str == NULL || *str == '\0')
+	return (NULL);
+n = countWordNum(str);
+
+if (n == 1)
+	return (NULL);
+ptr = (char **)malloc(n * sizeof(char *));
+if (ptr == NULL)
+	return (NULL);
+ptr[n - 1] = NULL;
+i = 0;
+while (str[i])
+{
+
+if (str[i] != ' ' && (i == 0 || str[i - 1] == ' ' ))
+{
+for (j = 1; str[i + j] != ' '; && str[i + j]; j++)
+	;
+j++;
+ptr[wc] = (char *)malloc(j * sizeof(char));
+j--;
+if(ptr[wc] == NULL)
+{
+for (k = 0; k < wc; k++)
+	free(ptr[k]);
+free(ptr[n - 1]);
+free(ptr);
+return (NULL);
+}
+
+for (l = 0; l < j; l++)
+        ptr[wc][l] = str[i + l];
+ptr[wc][l] = '\0';
+wc++;
+i += j;
+}
+else
+{
+i++;
+}
+}
+return (ptr);
 }
