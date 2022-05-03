aa8c375e4f8a3eba4ebbe6dcc04c411590c5e098
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-03 10:07:00 -0700

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - a function that returns a copy of the string
+ * @str: a pointer for the given string
+ *
+ * Return: return a new copy of @str
+ */
+
+char *_strdup(char *str)
+{
+char *st = NULL;
+st = malloc(strlen(str) + 1);
+if (st != NULL)
+	strcpy(st, str);
+return (st);
+}
