caff6ef78a8a1b90a30ff2a49787eaa2d4ed750a
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-05-03 21:44:32 +0300

    Task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,27 +12,36 @@
 
 int **alloc_grid(int width, int height)
 {
-if (width <= 0 || height <= 0)
-	return (NULL);
-int **ptr;
-int i, j;
-ptr = malloc(height * sizeof(int *));
-if (ptr == NULL)
-	return (NULL);
+	int **ptr;
+	int i, j;
 
-for (i = 0; i < height; i++)
-{
-ptr[i] = malloc(sizeof(int) * width);
-if (ptr[i] == NULL)
-	for (i = i - 1; i >= 0; i--)
-		free(ptr[i]);
-free(ptr);
-return (NULL);
-}
-for (i = 0; i < height; i++)
-{
-for (j = 0; j < width; j++)
-	ptr[i][j] = 0;
-}
-return (ptr);
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	ptr = malloc(sizeof(int *) * height);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = malloc(sizeof(int) * width);
+
+		if (ptr[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(ptr[i]);
+
+			free(ptr);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			ptr[i][j] = 0;
+	}
+
+	return (ptr);
 }
