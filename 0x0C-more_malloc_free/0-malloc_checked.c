920967522e17e6f6daa5d6e30d4e93b68f1605a4
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-06 10:21:55 -0700

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,13 +9,11 @@
  */
 void *malloc_checked(unsigned int b)
 {
-char *ptr;
-ptr = malloc(sizeof(b));
+void *ptr;
+ptr = malloc(b);
 
 if (ptr == NULL)
-{
-exit(98);
-return (NULL);
-}
+	exit(98);
+
 return (ptr);
 }
