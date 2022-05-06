27d9cfa52801c1bbd0d1b4d7e5b50f5535455c6d
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-05-06 22:05:43 +0300

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,33 +12,41 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	unsigned int i, len, j;
-	unsigned int size;
+	unsigned int sz1 = 0, sz2 = 0, i;
 
-	len = 0;
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[len] != '\0')
-		len++;
-	size = (len + n) * sizeof(*ptr);
-	ptr = malloc(size + 1);
+
+	while (s1[sz1] != '\0')
+	{
+		sz1++;
+	}
+
+	while (s2[sz2] != '\0')
+	{
+		sz2++;
+	}
+
+	if (n > sz2)
+	n = sz2;
+	ptr = malloc((sz1 + n + 1) * sizeof(char));
+
 	if (ptr == NULL)
-		return (NULL);
-	i = 0;
-	while (i < size && s1[i] != '\0')
+		return (0);
+
+	for (i = 0; i < sz1; i++)
 	{
 		ptr[i] = s1[i];
-		i++;
 	}
-	j = 0;
-	while (i < size && s2[j] != '\0')
+
+	for (; i < (sz1 + n); i++)
 	{
-		ptr[i] = s2[j];
-		i++;
-		j++;
+		ptr[i] = s2[i - sz1];
 	}
 	ptr[i] = '\0';
-	return (ptr);
+
+return (ptr);
 }
