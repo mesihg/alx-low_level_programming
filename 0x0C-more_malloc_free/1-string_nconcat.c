4a608c8e42ad7577e9c1b09833c1cc89465b1153
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-05-06 22:01:03 +0300

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,18 +11,34 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *ptr;
-unsigned int i, j, k;
+	char *ptr;
+	unsigned int i, len, j;
+	unsigned int size;
 
-if (s1 == NULL)
-	s1 = "";
-if (s2 == NULL)
-	s2 = "";
-
-while (s1[i] != '\0')
-	i++;
-while (s2[j] != '\0')
-	j++;
-
-if (n > j)
+	len = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[len] != '\0')
+		len++;
+	size = (len + n) * sizeof(*ptr);
+	ptr = malloc(size + 1);
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size && s1[i] != '\0')
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (i < size && s2[j] != '\0')
+	{
+		ptr[i] = s2[j];
+		i++;
+		j++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
 }
