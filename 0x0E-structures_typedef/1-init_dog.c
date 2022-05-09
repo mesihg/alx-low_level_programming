6c40cc535b72e56d2db2e857819a47bb7d05a330
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-09 06:29:57 -0700

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * intit_dog - initialize the variable of type struct dog
+ * @d: pointer to dog struct
+ * @name: pointer for name
+ * @age: age of the dog struct
+ * @owner: pointer for owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+		{
+			d->name = name;
+			(*d).age = age;
+			d->owner = owner;
+		}
+}
