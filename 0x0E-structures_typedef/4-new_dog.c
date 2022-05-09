a19b6a9e60ccad9a688dffb23d5f478fdc4cfcd1
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-05-09 22:31:59 +0300

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,43 +1,76 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
- * new_dog - creates a copy of new dog
- * @name: pointer for name
- * @age: age of the dog struct
- * @owner: pointer for owner
+ * new_dog - create new data structure for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
  *
- * Return: returns a new copy of struct dog
- */
+ * Return: Pointer to new dog
+ **/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *ptr;
-unsigned int i, len_name, len_owner;
+	dog_t *new_dog;
+	int len_name, len_owner;
 
-len_name = strlen(name);
-len_owner = strlen(owner);
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
 
-ptr = malloc(sizeof(dog_t));
-if (ptr == NULL)
-	return (NULL);
+	len_name = _strlen(name);
+	new_dog->name = malloc(sizeof(char) * len_name + 1);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->name = _strcpy(new_dog->name, name);
+	len_owner = _strlen(owner);
+	new_dog->owner = malloc(sizeof(char) * len_owner + 1);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
 
-ptr->name = malloc(sizeof(char) * len_name);
-if (ptr->name == NULL)
-{
-free(ptr);
-return (NULL);
+	new_dog->owner = _strcpy(new_dog->owner, owner);
+	new_dog->age = age;
+
+	return (new_dog);
 }
-ptr->name = strcpy(ptr->name, name);
-ptr->owner = malloc(sizeof(char) * len_owner);
-if (ptr->owner == NULL)
+
+/**
+ * _strlen - determinates the lenght of a string
+ * @s: pointer to string
+ * Return: the length
+ */
+int _strlen(char *s)
 {
-free(ptr);
-free(ptr->name);
-return (NULL);
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	;
+	return (a);
 }
 
-ptr->owner = strcpy(ptr->owner, owner);
-ptr->age = age;
-return (ptr);
+/**
+ * _strcpy - copies a pointed string
+ * @dest: pointer to the destine string
+ * @src: pointer to the source string
+ * Return: the adress of the destiny string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+
+	while (src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	dest[a] = '\0';
+	return (dest);
 }
