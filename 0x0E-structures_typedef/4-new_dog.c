4ba1f839bf1c5c9ae3ed212328f31a03d79264af
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-09 10:32:22 -0700

    Task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a copy of new dog
+ * @name: pointer for name
+ * @age: age of the dog struct
+ * @owner: pointer for owner
+ *
+ * Return: returns a new copy of struct dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *ptr;
+
+ptr = malloc(sizeof(dog_t));
+
+if (ptr == NULL)
+	return (NULL);
+
+ptr->name = name;
+ptr->age = age;
+ptr->owner = owner;
+
+return (ptr);
+}
