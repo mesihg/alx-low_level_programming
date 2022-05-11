3a7f34de60bd8da0ff77ac4875a8ed2380a9618c
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-11 10:24:27 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+
 /**
  * print_name - prints a name
  * @name: a pointer for name
@@ -9,6 +10,8 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if(name && f)
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+	
+	f(name);
 }
