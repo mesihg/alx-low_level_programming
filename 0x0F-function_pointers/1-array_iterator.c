1f95f6cf5634a8c4b179984094fb70e0a74bed0e
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-11 11:16:16 -0700

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,10 +9,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-int i;
-if (array != NULL && action != NULL)
+unsigned int i;
+
+if (array && size > 0 && action)
 {
 for (i = 0; i < size; i++)
-	action(array[i]);
+{
+action(array[i]);
+}
 }
 }
