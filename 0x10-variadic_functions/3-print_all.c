f3869643c7a6c5f693db37eaf05b7496a9777bb2
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-12 13:20:30 -0700

    Task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,52 +8,59 @@
  */
 void print_all(const char *const format, ...)
 {
-va_list list;
-unsigned int i = 0, j, k = 0;
-char *str;
-const char op_arg[] = "cifs";
-va_start(list, format);
+	va_list list;
+	unsigned int i = 0, j, k = 0;
+	char *str;
+	const char op_arg[] = "cifs";
+	va_start(list, format);
+	
+	while (format && format[i])
+		{
+			j = 0;
+			while (op_arg[j])
+				{
+					if (format[i] == op_arg[j] & k)
+						{
+							printf(", ");
+							break;
+							
+						}
+					j++;
 
-while (format && format[i])
-{
-j = 0;
-while (op_arg[j])
-{
-if (format[i] == op_arg[j] & k)
-{
-printf(", ");
-break;
-}
-j++;
-}
-switch (format[i])
-{
-    case 'c':
-        printf("%c", va_arg(list, int));
-        k = 1;
-        break;
-    case 'i':
-        printf("%d", va_arg(list, int));
-        k = 1;
-        break;
-    case 'f':
-        printf("%f", va_arg(list, double));
-        k = 1;
-        break;
-    case 's':
-        str = va_arg(list, char *);
-        k = 1;
-        if (str != NULL)
-        {
-            printf("(nil)");
-            break;
-        }
-        printf("%s", str);
-        break;
-}
-i++;
-}
+					
+				}
 
-printf("\n");
-va_end(valist);
+			switch (format[i])
+				{
+				    case 'c':
+					    printf("%c", va_arg(list, int));
+					    k = 1;
+					    break;
+			            case 'i':
+					    printf("%d", va_arg(list, int));
+					    k = 1;
+					    break;
+			            case 'f':
+					    printf("%f", va_arg(list, double));
+					    k = 1;
+					    break;
+		                    case 's':
+					    str = va_arg(list, char *);
+					    k = 1;
+					    if (str == NULL)
+						    {
+							    printf("(nil)");
+							    break;	    
+						    }
+					    printf("%s", str);
+					    break;
+					    
+				}
+			i++;
+			
+		}
+	printf("\n");
+	va_end(valist);
+
+	
 }
