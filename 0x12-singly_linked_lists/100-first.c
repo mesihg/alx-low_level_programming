6267e71242bcb95e1aebda8f3c7f0742394ca564
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-19 11:33:21 -0700

    Task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
+void first(void) __attribute__ ((constructor));
 /**
  * first - prints a sentence
  */
 
-void first(void) __attribute__ ((constructor))
+void first(void)
 {
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
