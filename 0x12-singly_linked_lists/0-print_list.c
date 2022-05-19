87c1817898bb7c029c9f65fd576c640c5286faa3
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-19 07:07:24 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,18 +10,18 @@
 size_t print_list(const list_t *h)
 {
 
-size_t count;
+size_t count = 0;
 
-count = 0;
 
 while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		count++;
-	}
+{
+if (h->str == NULL)
+	printf("[%d] %s\n", 0, "(nil)");
+else
+	printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+count++;
+
+}
 return (count);
 }
