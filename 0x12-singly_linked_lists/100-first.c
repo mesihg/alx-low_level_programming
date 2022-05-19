4df8d61deea4d8b756ae7cac0f48c8dfa49bdca6
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-19 11:23:53 -0700

    Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * first - prints a sentence
+ */
+
+void first(void) __attribute__ ((constructor))
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
