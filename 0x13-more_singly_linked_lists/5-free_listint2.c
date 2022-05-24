f6d11821cbec322c710c5ed98c7da10d5c2b878a
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-24 11:54:39 -0700

    Task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,15 +9,16 @@
 
 void free_listint2(listint_t **head)
 {
-listint_t *ptr, *temp;
-if (head != NULL)
-{
-	ptr = *head;
-	while ((temp = ptr) != NULL)
+	listint_t *ptr, *temp;
+
+	if (head != NULL)
+	{
+		ptr = *head;
+		while ((temp = ptr) != NULL)
 		{
 			ptr = ptr->next;
 			free(temp);
 		}
-	*head = NULL;
-}
+		*head = NULL;
+	}
 }
