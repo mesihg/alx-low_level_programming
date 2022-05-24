2a58f365aa173bfad464121ca35636a09d8b5dda
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-24 10:45:44 -0700

    Task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,7 +10,7 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-listint_t new_node;
+listint_t *new_node;
 new_node = (listint_t *)malloc(sizeof(listint_int));
 if (new_node == NULL)
 	return (NULL);
