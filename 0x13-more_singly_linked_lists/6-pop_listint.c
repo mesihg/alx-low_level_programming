7472e397c4a74511921adfef888780af2f6ff67f
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-24 12:50:22 -0700

    Task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,14 +9,14 @@
 
 int pop_listint(listint_t **head)
 {
-	int data;
 	listint_t *ptr;
-	if (*head == NULL)
-		return (0);
+	int data;
 
+	if (!(*head) || !head)
+		return (0);
 	ptr = *head;
 	data = ptr->n;
-	*head = *head->next;
+	*head = (*head)->next;
 	free(ptr);
 	return (data);
 }
