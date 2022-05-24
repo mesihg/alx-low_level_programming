5deb469fc1f9a607b4c4658b178ac790b0908f3e
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-24 11:13:40 -0700

    Task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * free_listint2 - frees a listint_t list
+ * *head: pointer to head
+ *
+ * Return: nothing is returned
+ */
+
 void free_listint2(listint_t **head)
 {
 listint_t *ptr;
