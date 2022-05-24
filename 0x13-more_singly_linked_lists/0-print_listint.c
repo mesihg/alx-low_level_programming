184f5fbd8060313fa7713b043be0840630ae596a
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-24 06:49:51 -0700

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-size_t n;
-n = 0;
+size_t n = 0;
+
 while (h != NULL)
 {
-printf("%d\n", h->n);
-n++;
+printf("%i\n", h->n);
 h = h->next;
+n++;
 }
 return (n);
 }
