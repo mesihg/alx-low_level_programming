8251972b676e494502d72025a802e19ac69519fa
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-26 12:20:25 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,19 +10,25 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec, len, number;
+	if (b == NULL)
+	{
+		return (0);
+	}
 	number = 0;
 	dec = 1;
 	len = strlen(b) - 1;
 
-	if (b == NULL)
-		return (0);
 	while (len >= 0)
 	{
 		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
+		}
 
 		if (b[len] == '1')
+		{
 			number += dec;
+		}
 		dec *= 2;
 		len--;
 	}
