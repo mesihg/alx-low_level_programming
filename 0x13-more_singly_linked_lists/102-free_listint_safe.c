c6da17ba7c288ef1bf9a603f1e4845734102a425
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-25 20:03:45 -0700

    Task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -9,21 +9,5 @@
 
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current, *hold;
-	size_t count;
 
-	count = 0;
-	current = *h;
-	while (current != NULL)
-	{
-		count++;
-		hold = current;
-		current = current->next;
-		free(hold);
-		if (hold < current)
-			break;
-	}
-	*h = NULL;
-
-	return (count);
 }
