e0adf7d11c1e7f5faba871e644473c8c2714a8b7
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-26 12:28:10 -0700

    Task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary representation
+ * @n: binary input number
+ *
+ * Return: nothign is returned
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+	unsigned long int current;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = n >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+
+	if (!count)
+		_putchar('0');
+}
