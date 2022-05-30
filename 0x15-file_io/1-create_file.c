b1c09226550e34a410575a7d0788ec8d394431ba
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-30 10:48:40 -0700

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - create a file
+ * @filename: name of the file
+ * @text_content: contents to be written to the created file
+ *
+ * Return: 1 on success -1 otherwise
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+
+        int fd, len;
+        
+        if (!filename)
+	{
+		return (-1);
+	}
+	if (!text_content)
+	{
+		text_content = NULL;
+	}	       
+	fd = open(filename, O_CREATE | O_WRONLY | O_TRUNC, 0600);
+        
+	if (fd == -1)
+        {
+        return (-1);
+        }
+	while (text_content[i] != '\0')
+	{
+	len++;
+	}
+
+	write(fd,text_content,len);
+
+        close(fd);	
+	return (1);
+}
