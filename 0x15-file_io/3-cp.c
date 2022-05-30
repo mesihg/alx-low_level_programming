f7e9d8d0e6eb0d05ce939f8d3abb8ee71e4aac64
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-05-30 23:03:07 +0300

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,11 +20,10 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	
 	if (file_to == -1)
         {
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-                exit(99);
+          dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+          exit(99);
         }
 	nchar_read = 1024;
 	while (nchar_read == 1024)
