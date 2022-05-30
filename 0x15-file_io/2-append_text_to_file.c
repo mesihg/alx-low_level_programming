fc9ad9cf7e4dd0d61b6dab0a521be2dd7db2289c
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-05-30 21:48:52 +0300

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,5 +10,28 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fd, len, w_fd;
 
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (len = 0; text_content[len]; len++)
+		;
+
+	w_fd = write(fd, text_content, len);
+
+	if (w_fd == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
