641c5ddb877adb0d0eccefb945c0cffec9c0039f
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-30 11:53:51 -0700

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,16 +20,15 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
-		return;
+	if (text_content)
+	{
+		for (len = 0; text_content[len]; len++);
+		
+		w_fd = write(fd, text_content, len);
 
-	for (len = 0; text_content[len]; len++)
-		;
-
-	w_fd = write(fd, text_content, len);
-
-	if (w_fd == -1)
-		return (-1);
+		if (w_fd == -1)
+			return (-1);
+	}
 
 	close(fd);
 
