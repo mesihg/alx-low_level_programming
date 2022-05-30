0745acb366e5935b95765d864688644148ba6f12
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-05-30 21:01:54 +0300

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,18 +12,15 @@ int create_file(const char *filename, char *text_content)
 {
         int fd, len;
         if (!filename)
-	{
 		return (-1);
-	}
+	
 	if (!text_content)
-	{
 		text_content = NULL;
-	}
+	
 	fd = open(filename, O_CREATE | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
-        {
 		return (-1);
-        }
+	
 	while (text_content[i] != '\0')
 	{
 	len++;
