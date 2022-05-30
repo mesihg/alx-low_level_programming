5d932b6b7ae9f56c922ed1703592c93e1b5d7d76
Mesih Grmay <mesihgrmay@yahoo.com>
2022-05-30 11:46:59 -0700

    Task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of the file
+ * @text_content: contents to be appended to the created file
+ *
+ * Return: 1 on success -1 otherwise
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+
+}
