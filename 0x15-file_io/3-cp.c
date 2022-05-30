4839adadb28898c400bc4550cd8849340701de1c
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2022-05-30 23:10:17 +0300

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,24 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+* display_error_message - prints error message to the stderr screen
+*/
+
+void display_error_message(int file_from, int file_to, char *arg[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arg[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+        {
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", arg[2]);
+		exit(99);
+        }
+	
+}
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, nchar_read, nw_fd, er_cls;
@@ -14,37 +32,17 @@ int main(int argc, char *argv[])
 
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (file_to == -1)
-        {
-          dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-          exit(99);
-        }
+        display_error_message(file_from, file_to, argv);
+	
 	nchar_read = 1024;
 	while (nchar_read == 1024)
 	{
 		nchar_read = read(file_from, buff, 1024);
-
 		if (nchar_read == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-                        exit(98);
-		}
-
+			display_error_message(-1, 0, argv);
 		nw_fd = write(file_to, buff, nchar_read);
-
 		if (nw_fd == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-                        exit(99);
-		}
-
-
+			display_error_message(0, -1, argv);
 	}
 
 	err_cls = close(file_from);
