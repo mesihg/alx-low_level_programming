2fc6213e200ec47624ed3fb61836fe50f0954038
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-07 13:35:23 -0700

    Task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: pointer for head
+ * Return: nothing is returned
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
