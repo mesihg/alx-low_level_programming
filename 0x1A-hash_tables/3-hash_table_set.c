0fa2e4080c0dfe4773821e03cc7ab70a2b1d206b
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-29 08:57:02 -0700

    Task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: pointer to the hash table
+ * @key: key of the element to be added
+ * @value: value of the element to be added
+ *
+ * Return: 1 if it succeeded 0 otherwise
+ */
+
+ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+ {
+	 hash_node_t *new_node;
+	 unsigned long index, i;
+
+	 if (ht == NULL || key == NULL || value == NULL)
+		 return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
+	{
+		ht->array[index]->value = strdup(value);
+		return (1);
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+
+ }
