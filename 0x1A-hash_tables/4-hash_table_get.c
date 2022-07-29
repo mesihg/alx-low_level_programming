ad96fd64969a0b3998c6c46b39182b774b2bbc17
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-29 09:27:24 -0700

    Task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,7 +13,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *node;
 
-	if (ht == NULL || key == NULL && *key == '\0')
+	if (ht == NULL || key == NULL)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
