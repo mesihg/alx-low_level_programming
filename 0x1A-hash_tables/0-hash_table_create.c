5ade0e6c7f68a2241faafd53e68fcd32e9c60097
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-29 07:18:12 -0700

    Task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create a hash table
+ * @size: size of the array
+ * Return: newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *h_table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+		return (NULL);
+
+	h_table->size = size;
+	h_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (h_table->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		h_table->array[i] = NULL;
+	}
+	return (h_table);
+}
