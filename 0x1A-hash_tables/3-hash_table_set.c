f8ae24f37493c4181ed307a0d6f20bef6b4cd6c7
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-29 08:59:33 -0700

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@
  int hash_table_set(hash_table_t *ht, const char *key, const char *value)
  {
 	 hash_node_t *new_node;
-	 unsigned long index, i;
+	 unsigned long index;
 
 	 if (ht == NULL || key == NULL || value == NULL)
 		 return (0);
